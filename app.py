b9d66bec690f337d085966cdb714584566beb476
Umarahmad87 <umarahmadsiddiqi@outlook.com>
2021-08-13 01:44:57 +0500

    included models

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
+from bs4 import BeautifulSoup
+
 import requests
+
+import models
 
 # get all the articles list from api
 data = requests.get('https://mapping-test.fra1.digitaloceanspaces.com/data/list.json')
@@ -15,10 +19,27 @@ for record in data.json():
     # get all sections of particular article
     for section in article['sections']:
         if section.get('text', None):
-            print(section['text'])
+            soup = BeautifulSoup(section['text'])
+            section['text'] = soup.get_text()
+
+            if section['type'] == 'title':
+                title = models.TitleSection(**section)
+                print(title)
+            elif section['type'] == 'text':
+                text = models.TextSection(**section)
+                print(text)
 
         if section['type'] == 'media':
             for media in media_list:
                 if media['id'] == section['id']:
-                    print(media['url'])
-                    print(media['caption'])
+
+                    if media['type'] == 'media':
+                        date_split = media['pub_date'].split('-')
+                        date = '-'.join(date_split[0:3])
+                        time = ':'.join(date_split[-1].split(';'))
+                        media['pub_date'] = f'{date}T{time}'
+                        media_data = models.MediaSection(**media)
+                        print(media_data)
+                    elif media['type'] == 'image':
+                        image = models.ImageSection(**media)
+                        print(image)
